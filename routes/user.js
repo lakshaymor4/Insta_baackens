e97102436d7bcec081ed302f292575c038965dfe
lakshaymor4 <morwal.lakshay2022@vitstudent.ac.in>
2024-05-28 13:11:05 +0530

    new router defined

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,9 @@ const Login = require("../controllers/login");
 const verifyJWT = require("../server/verifyjwt");
 const protected = require("../controllers/protected_route");
 const otp = require("../controllers/otp");
+router.get("/", (req, res) => {
+  res.send("Welcome to the user route");
+});
 router.post("/signup", Register);
 router.post("/login", Login);
 router.get("/", verifyJWT, protected);
